e86982911f613026a9ce34b72573477bb73fbde0
cazaroni <jorday.02@gmail.com>
2025-11-25 20:53:20 -0700

    test 5

--- algorithms.py
+++ algorithms.py
@@ -1,10 +1,17 @@
 # Algorithm definitions for calenderization simulation
+# processes are defined as simple python lists, no additional classes needed to manipulate them for our use
 
 def FCFS(processes):
     """First-Come, First-Served scheduling algorithm"""
+    # sorts list by the arrival time (attribute is found within the class itself)
+    processes.sort(key=lambda x: x.arrival)
+    # to continue cuz i dont want to do this on my laptop
+
+
 
 def SJF(processes):
     """Shortest Job First scheduling algorithm"""
+    processes.sort(key=lambda x: x.burst)
     example = False
 
 
